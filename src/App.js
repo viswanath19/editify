3684442e9d64ee01eba530f0a09b34f612940f4d
Viswanath <allamrajuviswanath@gmail.com>
2023-08-10 23:48:18 +0530

    webpack setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 import TextEditorComponent from './components/EditorComponent/TextEditorComponent';
 
 const App = () => {
